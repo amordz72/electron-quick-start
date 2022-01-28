17cb2d5b58cb2be0de2c5c31556cb7d11c2c2a22
amor <50753427+amordz72@users.noreply.github.com>
2022-01-28 13:03:28 +0100

    fill proj & tbls

--- views/assets/js/dashboard_App.js
+++ views/assets/js/dashboard_App.js
@@ -1,52 +1,85 @@
-const app = Vue.createApp({
-    props: ["title"],
-    data() {
-      return {
-        project:"",
-        allData: "",
-        _url: "https://localhost/formAssi/assets/api/dashbord.php",
-        count_lang: 0,
-        count_users: 0,
-      };
-    },
-    computed: {},
-    methods: {
-      get_count: function () {
+const app = {
+  props: ["title"],
+  data() {
+    return {
+      all_projs: null,
+      all_tables: null,
+      project_id: 0,
+      project: "",
+      allData: "",
+      url: "http://localhost/fc_api/public/api/",
+      count_lang: 0,
+      count_users: 0,
+    };
+  },
+  computed: {},
+  methods: {
+    all_p: function () {
+      try {
         var me = this;
-        axios
-          .post(me._url, {
-            action: "count_lang",
-          })
-          .then(function (response) {
-            me.count_lang = response.data.count_lang;
-          });
-      },
-      get: function (table, col, sub, control) {
+
+        axios.get(me.url + 'project', {
+
+        }).then(function (response) {
+          me.all_projs = response.data;
+
+        });
+      } catch (error) {
+        console.log(error);
+      }
+    },
+    all_tbls: function () {
+      try {
         var me = this;
-        axios
-          .post(me._url, {
-            action: "get",
-            table: table,
-            col: col,
-            sub: sub,
-          })
-          .then(function (response) {
-            if (control == "count_lang") {
-              me.count_lang = response.data.sub;
-              console.log(response.data.sub);
-            }
-           else if (control == "count_users") {
-              me.count_users = response.data.sub;
-            }
-          });
-      },
+ 
+        axios.get(me.url + 'table/' +   me.project_id  , {
+
+        }).then(function (response) {
+          me.all_tables = response.data;
+
+        });
+      } catch (error) {
+        console.log(error);
+      }
     },
-    created() {
-      // this.fetchAllTables(); this.getJsonData();
+
+    get_count: function () {
+      var me = this;
+      axios
+        .post(me._url, {
+          action: "count_lang",
+        })
+        .then(function (response) {
+          me.count_lang = response.data.count_lang;
+        });
     },
-    mounted() {
-      this.get("lang", "laId", "max", "count_lang");
-      this.get("users", "usId", "max", "count_users");
+    get: function (table, col, sub, control) {
+      var me = this;
+      axios
+        .post(me._url, {
+          action: "get",
+          table: table,
+          col: col,
+          sub: sub,
+        })
+        .then(function (response) {
+          if (control == "count_lang") {
+            me.count_lang = response.data.sub;
+            console.log(response.data.sub);
+          }
+          else if (control == "count_users") {
+            me.count_users = response.data.sub;
+          }
+        });
     },
-  });
-  
+  },
+  created() {
+    // this.fetchAllTables(); this.getJsonData();
+  },
+  mounted() {
+    this.all_p();
+
+  },
+}
+
+Vue.createApp(app).mount('#app')
